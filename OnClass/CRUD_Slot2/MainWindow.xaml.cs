aab0d03a348fd2351fcd444d6d008fa2666f920c
Phanhgit config --global user.name Phanh <kieuphuonganh1112@gmail.com>
2024-09-22 15:12:58 +0700

    update code

---
+++ OnClass/CRUD_Slot2/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using CRUD_Slot2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CRUD_Slot2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void load()
+        {
+            var student = PRN221_1Context.Ins.Students.Include(x => x.Depart).Select(x => new
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Gender = x.Gender ? "Male":"Female",
+                DepartId = x.DepartId,
+                Dob = x.Dob,
+                Gpa = x.Gpa,
+                Depart = x.Depart
+            }).ToList();
+            dgvDisplay.ItemsSource = student;
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            load();
+            loadDepart();
+            cbDepartFilter.SelectedIndex = 0;
+        }
+        private void loadDepart()
+        {
+            var dept = PRN221_1Context.Ins.Departments.Select(x => x.Name).ToList();
+            cbDepartFilter.ItemsSource = dept;
+        }
+
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            string dept = cbDepartFilter.SelectedValue.ToString();
+            string deptId = PRN221_1Context.Ins.Departments.FirstOrDefault(x => x.Name.Equals(dept)).Id;
+            var st = PRN221_1Context.Ins.Students.Include(x => x.Depart).Where(x => x.DepartId.Equals(deptId)).ToList();
+            dgvDisplay.ItemsSource = st;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string txt = txtSearch.Text;
+            
+        }
+    }
+    //BTVN dinh dang lai db teo dd/mm/yyyy
+    //thay the datagrid --> listview
+    //filter theo gender (Male/Female/All)
+    //Dockpanel -left-right
+    //chia deu thanh ba cot grid
+}
---
+++ OnClass/CRUD_Slot2/Models/PRN221_1Context.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+
+namespace CRUD_Slot2.Models
+{
+    public partial class PRN221_1Context : DbContext
+    {
+        public static PRN221_1Context Ins = new PRN221_1Context();
+        public PRN221_1Context()
+        {
+            if (Ins == null) Ins = this;
+        }
+
+        public PRN221_1Context(DbContextOptions<PRN221_1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            if (!optionsBuilder.IsConfigured) { optionsBuilder.UseSqlServer(config.GetConnectionString("value")); }
+            //            if (!optionsBuilder.IsConfigured)
+            //            {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            //                optionsBuilder.UseSqlServer("Data Source=PHANH\\PANH;Initial Catalog=PRN221_1; Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
+            //            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.ToTable("Department");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(10)
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(30)
+                    .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.DepartId)
+                    .HasMaxLength(10)
+                    .HasColumnName("departId");
+
+                entity.Property(e => e.Dob)
+                    .HasColumnType("date")
+                    .HasColumnName("dob");
+
+                entity.Property(e => e.Gender).HasColumnName("gender");
+
+                entity.Property(e => e.Gpa).HasColumnName("gpa");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(30)
+                    .HasColumnName("name");
+
+                entity.HasOne(d => d.Depart)
+                    .WithMany(p => p.Students)
+                    .HasForeignKey(d => d.DepartId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Student_Department");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
